5467b552683f77968b4d2f5573873c5731aab1bd
Thirtybird <thirtybird@gmail.com>
2013-06-05 14:55:40 -0300

    Update yacoin.h

--- yacoin.h
+++ yacoin.h
@@ -8,10 +8,10 @@ pulled out of the original scrypt-jane.c in ali1234's repository
 */
 
 
-unsigned char GetNfactor(unsigned int nTimestamp)
+unsigned char GetNfactor(unsigned int nTimestamp);
 
 int scanhash_scrypt_jane(int thr_id, uint32_t *pdata,
         const uint32_t *ptarget,
-        uint32_t max_nonce, unsigned long *hashes_done)
+        uint32_t max_nonce, unsigned long *hashes_done);
 
 #endif /* __YACOIN_H__ */
